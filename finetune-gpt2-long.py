ef6692ded5697290a20b42d32cdcf4fc218e1546
VatsaDev <71975550+VatsaDev@users.noreply.github.com>
2023-10-02 12:03:15 -0500

    Add files via upload

---
+++ finetune-gpt2-long.py
@@ -0,0 +1,22 @@
+import time
+
+out_dir = '/content/drive/MyDrive/Model' # for google colab, for regular use, set value to 'out-chat'
+eval_interval = 5
+eval_iters = 200
+wandb_log = False # feel free to turn on
+wandb_project = 'gpt2finetune'
+wandb_run_name = 'ft-' + str(time.time())
+
+dataset = ''
+init_from = 'gpt2-medium' # good for google colab
+
+# only save checkpoints if the validation loss improves
+always_save_checkpoint = False
+
+batch_size = 4
+gradient_accumulation_steps = 32
+max_iters = 200
+
+# finetune at constant LR
+learning_rate = 3e-4
+decay_lr = False
